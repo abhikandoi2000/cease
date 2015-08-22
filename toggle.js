d44637b773f247a7fcd71ca50ef9d15c584275c2
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-08-22 17:18:31 +0530

    Updates code for Youtube
    Youtube shifted to a different version of their player

--- toggle.js
+++ toggle.js
@@ -1,17 +1,8 @@
-var pauseButton = document.querySelectorAll('.ytp-button-pause')[0];
-var playButton  = document.querySelectorAll('.ytp-button-play')[0];
+var playPauseButton = document.querySelectorAll('.ytp-play-button')[0];
 
-console.log(pauseButton);
+console.log(playPauseButton);
 
-if(typeof(pauseButton) != "undefined") {
-  window.localStorage.playing = true;
+if(typeof(playPauseButton) != "undefined") {
+  playPauseButton.click();
 }
 
-
-if(window.localStorage.playing) {
-  if(pauseButton) {
-    pauseButton.click();
-  } else if(playButton) {
-    playButton.click();
-  }
-}
